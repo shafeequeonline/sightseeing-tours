c85bb99f69703e7457e9b2c860c4ab9fbf7dd8e1
Shafeeque <mshafeequemsn@gmail.com>
2021-06-30 00:42:20 +0530

    write test cases

--- src/components/Cabs/Cabs.test.js
+++ src/components/Cabs/Cabs.test.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from '@material-ui/core/Modal';
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import SimpleModal from "../SimpleModal/SimpleModal";
+import ReactDOM from "react-dom";
+import Header from "../Header";
+import Cabs from "./Cabs";
 
+configure({adapter: new Adapter()});
 describe('Cabs component', () => {
-    it('should load Modal component from Material', () => {
 
+    const MockCabs = [
+        {group: 'Premium'},
+        {group: 'Business'},
+        {group: 'First'},
+        {group: 'Business'},
+    ]
+
+    let premiumCabsMock, cabsMock;
+    beforeEach(() => {
+        premiumCabsMock = MockCabs.filter(car => car.group === "Premium")
+        cabsMock = MockCabs.filter(car => car.group !== "Premium")
+    })
+
+    it('renders without crashing', () => {
+        expect(<Cabs/>).toBeTruthy()
+    });
+
+    it('should render Cabs', () => {
+        const MockCab = {group: 'Premium'}
+
+        const handleClose = () => {
+            return false
+        };
+
+        const wrapper = shallow(<Modal open={true} onClose={handleClose}><SimpleModal data={MockCab} /></Modal>)
+        expect(wrapper.exists()).toBe(true)
+    })
+
+    it('should get Premium cabs', () => {
+        expect(premiumCabsMock.length).toBe(1)
     });
 })
